689aa9f0f7d6912faf7671b6dc134c5ae323f173
turanct <spinnewebber_toon@hotmail.com>
2015-07-31 07:40:28 +0200

    We want to see travis build fail to be sure that it works...

--- src/Twitch/Follower/RealName.php
+++ src/Twitch/Follower/RealName.php
@@ -6,8 +6,7 @@ final class RealName
 {
     private $name;
 
-    public function __construct($name)
-    {
+    public function __construct($name) {
         $this->name = (string) $name;
     }
 }
