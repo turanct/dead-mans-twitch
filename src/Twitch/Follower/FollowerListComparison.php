9fd6a14ba7aa213732716d9752a3776cefdf2785
turanct <spinnewebber_toon@hotmail.com>
2015-08-26 15:56:51 +0200

    Add proper phpdoc to the FollowerListComparison::diff method

--- src/Twitch/Follower/FollowerListComparison.php
+++ src/Twitch/Follower/FollowerListComparison.php
@@ -4,6 +4,14 @@ namespace Twitch\Follower;
 
 final class FollowerListComparison
 {
+    /**
+     * Get removed followers between an old and a new list of followers
+     *
+     * @param Follower[] $old An old list of followers
+     * @param Follower[] $new A new list of followers
+     *
+     * @return Follower[] The removed followers
+     */
     public function diff(array $old, array $new)
     {
         $diff = array();
