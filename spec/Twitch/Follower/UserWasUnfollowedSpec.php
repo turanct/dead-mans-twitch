b985042126b3a0211afdc485755ea1f68bc972a2
turanct <spinnewebber_toon@hotmail.com>
2015-08-29 18:24:35 +0200

    The UserWasUnfollowed event should have immutable getters

---
+++ spec/Twitch/Follower/UserWasUnfollowedSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Twitch\Follower;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Twitch\Follower\ScreenName;
+use Twitch\Follower\UserId;
+use DateTime;
+
+class UserWasUnfollowedSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->beConstructedWith(
+            new ScreenName('@foobar'),
+            new UserId('1234'),
+            new DateTime('now')
+        );
+
+        $this->shouldHaveType('Twitch\Follower\UserWasUnfollowed');
+    }
+
+    function it_has_properties_we_can_get_but_not_mutate()
+    {
+        $screenName = new ScreenName('@foobar');
+        $followerId = new UserId('1234');
+        $date = new DateTime('now');
+
+        $this->beConstructedWith($screenName, $followerId, $date);
+
+        // All properties should be transparantly returned
+        $this->getScreenName()->shouldReturn($screenName);
+        $this->getFollowerId()->shouldReturn($followerId);
+        $this->getDate()->shouldBeLike($date);
+
+        // The stored date should not be modified, only the local copy
+        $this->getDate()->modify('+1 day');
+        $this->getDate()->shouldBeLike($date);
+    }
+}
--- src/Twitch/Follower/UserWasUnfollowed.php
+++ src/Twitch/Follower/UserWasUnfollowed.php
@@ -2,14 +2,40 @@
 
 namespace Twitch\Follower;
 
+use DateTime;
+
 final class UserWasUnfollowed
 {
     private $screenName;
     private $followerId;
+    private $date;
 
-    public function __construct(ScreenName $screenName, UserId $followerId)
-    {
+    public function __construct(
+        ScreenName $screenName,
+        UserId $followerId,
+        DateTime $date = null
+    ) {
         $this->screenName = $screenName;
         $this->followerId = $followerId;
+
+        if ($date === null) {
+            $date = new DateTime('now');
+        }
+        $this->date = clone $date;
+    }
+
+    public function getScreenName()
+    {
+        return $this->screenName;
+    }
+
+    public function getFollowerId()
+    {
+        return $this->followerId;
+    }
+
+    public function getDate()
+    {
+        return clone $this->date;
     }
 }
