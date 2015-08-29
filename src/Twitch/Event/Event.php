2efb336c858e52a26975c2b58429fcc3e2207aad
turanct <spinnewebber_toon@hotmail.com>
2015-08-29 19:10:02 +0200

    Make the Event interface enforce event serializability
    All events can be serialized to json strings

--- src/Twitch/Event/Event.php
+++ src/Twitch/Event/Event.php
@@ -10,4 +10,18 @@ interface Event
      * @return DateTime
      */
     public function getDate();
+
+    /**
+     * Serialize the event to a json representation
+     *
+     * E.g.:
+     * {
+     *   "event": "FooWasBarred",
+     *   "date": 1440867474,
+     *   "otherProperty": "foo"
+     * }
+     *
+     * @return string
+     */
+    public function toJson();
 }
